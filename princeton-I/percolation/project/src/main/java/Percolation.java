160e524d90334c5f410ed9f8e637e33701b19a40
Le Cong <le.cong@onestepride.com>
2020-06-26 20:22:37 -0400

    100%

--- princeton-I/percolation/project/src/main/java/Percolation.java
+++ princeton-I/percolation/project/src/main/java/Percolation.java
@@ -1,10 +1,9 @@
-import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
   private final int dimension;
-  private int[][] arr = null;
+  private boolean[][] arr = null;
   private int numberOfOpenSites = 0;
   private WeightedQuickUnionUF uf = null;
   private WeightedQuickUnionUF ufF = null;
@@ -18,7 +17,7 @@ public class Percolation {
       throw new IllegalArgumentException("n needs to be bigger than 0");
     }
     dimension = n;
-    arr = new int[n][n];
+    arr = new boolean[n][n];
     uf = new WeightedQuickUnionUF(n * n + 2);
     ufF = new WeightedQuickUnionUF(n * n + 1);
     virtualTopSiteIndex = n * n;
@@ -33,7 +32,7 @@ public class Percolation {
       return;
     }
 
-    arr[row - 1][col - 1] = 1;
+    arr[row - 1][col - 1] = true;
     numberOfOpenSites++;
     final int idx = (row - 1) * dimension + (col - 1);
     if (row > 1) {
@@ -83,7 +82,7 @@ public class Percolation {
   // is the site (row, col) open?
   public boolean isOpen(final int row, final int col) {
     checkInput(row, col);
-    return arr[row - 1][col - 1] == 1;
+    return arr[row - 1][col - 1] == true;
   }
 
   // is the site (row, col) full?
@@ -101,10 +100,16 @@ public class Percolation {
   // does the system percolate?
   public boolean percolates() {
     return uf.find(virtualBottomSiteIndex) == uf.find(virtualTopSiteIndex);
+    // for (int i = 1; i <= dimension; i++) {
+    // if (isFull(dimension, i)) {
+    // return true;
+    // }
+    // }
+    // return false;
   }
 
   // test client (optional)
   public static void main(final String[] args) {
-    StdOut.printf("[%.2f, %f]", (double) 0.2, (double) 0.3);
+    // StdOut.printf("[%.2f, %f]", (double) 0.2, (double) 0.3);
   }
 }
